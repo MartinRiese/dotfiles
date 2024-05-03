581b488fa0b52610187bb1e7ebad320a773f62e7
Martin Riese <martin.riese@gmail.com>
2024-05-03 16:37:30 -0500

    Add alias for picking from recent git branches

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -4,3 +4,5 @@ alias wttr="curl 'wttr.in/54601?u'"
 alias esup="sdk use java 8.0.322-zulu; elasticsearch"
 alias vim="nvim"
 alias vimk='NVIM_APPNAME="nvim-kickstart" nvim'
+# Pick from recent git branches. Based on https://gist.github.com/jordan-brough/48e2803c0ffa6dc2e0bd
+alias gitr='git reflog | head -n15 | egrep -io "moving from ([^[:space:]]+)" | cut -d " " -f 3 | awk '\'' !seen[$0]++'\'' | fzf | xargs git switch'
