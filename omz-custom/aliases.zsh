becb0bee7b34d38825534e1067a9c51974dee1d9
Martin Riese <martin.riese@gmail.com>
2024-11-13 11:30:05 -0600

    rename atuin.zsh to tools.zsh and add fzf config
    * fzf config above to make sure right tools is bound to ctrl-r

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -5,4 +5,4 @@ alias esup="sdk use java 8.0.322-zulu; elasticsearch"
 # alias vim="nvim"
 alias vim='NVIM_APPNAME="nvim-kickstart" nvim'
 # Pick from recent git branches. Based on https://gist.github.com/jordan-brough/48e2803c0ffa6dc2e0bd
-alias gitr='git reflog | head -n15 | egrep -io "moving from ([^[:space:]]+)" | cut -d " " -f 3 | awk '\'' !seen[$0]++'\'' | fzf | xargs git switch'
+alias gitr='git reflog | head -n50 | egrep -io "moving from ([^[:space:]]+)" | cut -d " " -f 3 | awk '\'' !seen[$0]++'\'' | fzf | xargs git switch'
---
+++ omz-custom/tools.zsh
@@ -0,0 +1,7 @@
+# FZF
+source <(fzf --zsh)
+alias cdf="fzf-cd-widget"
+
+# Atuin
+. "$HOME/.atuin/bin/env"
+eval "$(atuin init zsh)"
