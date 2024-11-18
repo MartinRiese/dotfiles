c2417e3fd92848e812cbd6e3fc3b44ec88e2fbfa
Martin Riese <martin.riese@gmail.com>
2024-11-17 18:34:21 -0600

    Add shortcut for creating new branches

--- omz-custom/functions.zsh
+++ omz-custom/functions.zsh
@@ -2,3 +2,7 @@ function lint-branch() {
     git diff --name-only $(git merge-base HEAD origin/master) | grep '.py$' | xargs flake8
     git diff --name-only $(git merge-base HEAD origin/master) | grep '.js$' | xargs eslint
 }
+
+function gitb() {
+    git switch -c "riese/$1"
+}
