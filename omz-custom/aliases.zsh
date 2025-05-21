3f83cd5d72e630387dd4cd1f1a1f99834cc6e1ab
Martin Riese <martin.riese@gmail.com>
2025-05-21 08:35:29 -0500

    Add alias for vu

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -6,3 +6,4 @@ alias esup="sdk use java 8.0.322-zulu; elasticsearch"
 alias vim='NVIM_APPNAME="nvim-kickstart" nvim'
 # Pick from recent git branches. Based on https://gist.github.com/jordan-brough/48e2803c0ffa6dc2e0bd
 alias gitr='git reflog | head -n50 | egrep -io "moving from ([^[:space:]]+)" | cut -d " " -f 3 | awk '\'' !seen[$0]++'\'' | fzf | xargs git switch'
+alias venv='if [[ -d .venv ]] ; then source .venv/bin/activate ; elif [[ -d venv ]] ; then source venv/bin/activate ; fi'
